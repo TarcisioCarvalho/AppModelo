8bd7efce556f6522539cc18445cdf6825e691de3
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-02-21 21:04:13 -0300

    commit

--- src/DEVIO.UI.Site/Startup.cs
+++ src/DEVIO.UI.Site/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -15,8 +16,7 @@ namespace DEVIO.UI.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().set
-            services.AddMvc().set
+          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
