f0ab3399e10306470b152de93a98a08d72e63657
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-01 16:09:06 -0300

    Começando com EntityFrameWork

---
+++ src/DEVIO.UI.Site/Data/MeuDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DEVIO.UI.Site.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options)
+            : base(options) 
+        {
+
+        }
+    }
+}
--- src/DEVIO.UI.Site/Startup.cs
+++ src/DEVIO.UI.Site/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +18,13 @@ namespace DEVIO.UI.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting(options => options.LowercaseUrls = true);
@@ -27,6 +36,8 @@ namespace DEVIO.UI.Site
                 options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
                 options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
             });
+            services.AddDbContext<MeuDbContext>(options => options.
+            UseSqlServer(Configuration.GetConnectionString("MeuDbContext")));
             services.AddMvc();
             services.AddTransient<IPedidoRepository,PedidoRepository>();
         }
