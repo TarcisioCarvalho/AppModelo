185b36f82f445d36f243b5da863a255a8f5d7967
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-02 12:07:01 -0300

    Começando Com Identity

---
+++ src/AspNetCoreIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AspNetCoreIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetCoreIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetCoreIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+            });
+        }
+    }
+}
