5a54d157ed632e6b5241fd0b80181b83ca92c966
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-12 20:09:44 -0300

    Ambientes de Execucao

--- src/AspNetCoreIdentity/Config/DependecyInjectionConfig.cs
+++ src/AspNetCoreIdentity/Config/DependecyInjectionConfig.cs
@@ -1,5 +1,9 @@
-﻿using AspNetCoreIdentity.Extensions;
+﻿using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Extensions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreIdentity.Config
@@ -10,6 +14,21 @@ namespace AspNetCoreIdentity.Config
         {
             services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
             return services; 
+        }
+
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services,
+            IConfiguration configuration
+            )
+        {
+            services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                   options.UseSqlServer(
+                       configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.
+            SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+            return services;
         }
     }
 }
