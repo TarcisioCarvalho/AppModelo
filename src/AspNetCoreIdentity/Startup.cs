5f334ef70be77f47a3edd8b4ae47da42078a93f0
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-13 14:00:41 -0300

    Ambientes de Execucao2

--- src/AspNetCoreIdentity/Startup.cs
+++ src/AspNetCoreIdentity/Startup.cs
@@ -7,18 +7,27 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace AspNetCoreIdentity
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment hostingEnvironment)
         {
-            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings{hostingEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+       
 
        
         public void ConfigureServices(IServiceCollection services)
